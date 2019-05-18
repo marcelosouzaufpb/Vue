aabe6ec9c67b041f860074a0bba77f8e77745323
Marcelo <marcelo.souza@dce.ufpb.br>
2019-05-18 18:18:13 -0300

    Primeiro projeto com componentes ja definidos

---
+++ 06-componentes/componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-app>
+		<app-menu />
+		<app-toolbar />
+		<v-content>
+			<app-carousel />
+		</v-content>
+		<app-footer />
+		
+	</v-app>
+</template>
+
+<script>
+// Importes components
+import Menu from './Menu.vue'
+import Toolbar from './Toobar.vue'
+import Carousel from './Carosel.vue'
+import Footer from './Footer.vue'
+export default {
+	// Components
+	components: {'app-footer': Footer,
+	'app-carousel': Carousel,
+	'app-toolbar': Toolbar,
+	'app-menu': Menu},
+	
+}
+</script>
