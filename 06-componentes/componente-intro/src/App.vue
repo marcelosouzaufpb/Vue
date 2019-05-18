9e513ebd01ccd24248b9c99e93223198c52ce127
Marcelo <marcelo.souza@dce.ufpb.br>
2019-05-18 15:35:19 -0300

    Componente-intro

---
+++ 06-componentes/componente-intro/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Contadores</h1>
+        <app-contador />
+        <app-contador />
+        <app-contador />
+    </div> 
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
