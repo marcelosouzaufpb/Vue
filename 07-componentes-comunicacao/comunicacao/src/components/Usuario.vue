1751ca81bdd450b4835e9323d5dc74ede6170ace
Marcelo <marcelo.souza@dce.ufpb.br>
2019-05-19 20:42:06 -0300

    Comunicao filho com filho

--- 07-componentes-comunicacao/comunicacao/src/components/Usuario.vue
+++ 07-componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -3,14 +3,18 @@
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
         <p>Nome é <strong>{{ nome }}</strong></p>
+        <p>Idade é <strong>{{ idade }}</strong></p>
         <button @click="alterarNome">Alterar Nome</button>
         <hr>
         <div class="componentes">
             <app-usuario-info 
                 v-bind:nome="nome"
+                v-bind:idade="idade"
                 @nomeMudou="nome = $event"
                 :reinicarFn="reiniciarNome" />
-            <app-usuario-editar />
+            <app-usuario-editar 
+                v-bind:idade="idade"
+                @idadeMudou="idade = $event" />
         </div>
     </div>
 </template>
@@ -23,7 +27,8 @@ export default {
     components: { AppUsuarioInfo, AppUsuarioEditar },
     data() {
         return {
-            nome: 'Pedro'
+            nome: 'Pedro',
+            idade: 21
         }
     },
     methods: {
