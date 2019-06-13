c3b0f33de3306357e79e9c7cba707944a973ea81
Marcelo <marcelo.souza@dce.ufpb.br>
2019-06-13 13:21:43 -0300

    Finalizando desafio

--- 10-formularios/formulario-desafio/src/App.vue
+++ 10-formularios/formulario-desafio/src/App.vue
@@ -1,96 +1,127 @@
 <template>
-	<div id="app">
-		<h1>Formulário Desafio</h1>
-		<div class="conteudo">
-			<form class="painel">
-				<div class="cabecalho">Formulário</div>
-				<!-- Exercicio 01 -->
-				<!-- Criar uma formulário de registro -->
-				<!-- Nome completo (Nome e Sobrenome) -->
-				<!-- Email -->
-				<!-- Senha -->
-				<!-- Armazenar Dados? (Sim/Não) -->
-
-				<!-- Exercicio 02 -->
-				<!-- Só mostrar o fomulário de não tiver sido submetido -->
-				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
-				
-				<!-- Exercicio 03 -->
-				<!-- Crie um componente personalizado NomeCompleto -->
-				<!-- Esse componente deve receber Nome e Sobrenome -->
-			</form>
-			<div class="painel">
-				<div class="cabecalho">Resultado</div>
-
-			</div>
-		</div>
-	</div>
+  <div id="app">
+    <h1>Formulário Desafio</h1>
+    <div class="conteudo">
+      <form class="painel" v-if="!enviado">
+        <div class="cabecalho">Formulário</div>
+       	<NomeCompleto v-model="nomeCompleto" />
+        <Rotulo email="Email">
+          <input type="email" v-model="email">
+        </Rotulo>
+        <Rotulo senha="Senha">
+          <input type="password" v-model="senha">
+        </Rotulo>
+        <Rotulo armazenarDados="Armazenar Dados?" v-model="armazenarDados">
+          <input type="checkbox">
+        </Rotulo>
+		<hr>
+		<button @click.prevent="enviar">Enviar</button>
+        <!-- Exercicio 03 -->
+        <!-- Crie um componente personalizado NomeCompleto -->
+        <!-- Esse componente deve receber Nome e Sobrenome -->
+      </form>
+      <div class="painel" v-else>
+        <div class="cabecalho">Resultado</div>
+			<Rotulo nome="Nome">
+				<samp>{{ nomeCompleto.nome }}</samp>
+			</Rotulo>
+			<Rotulo nome="Sobrenome">
+				<samp>{{ nomeCompleto.sobrenome }}</samp>
+			</Rotulo>
+			<Rotulo nome="Email">
+				<samp>{{ email }}</samp>
+			</Rotulo>
+			<Rotulo nome="Senha">
+				<samp>{{  senha }}</samp>
+			</Rotulo>
+			<Rotulo nome="Armazenar Dados?">
+				<samp>{{ armazenarDados }}</samp>
+			</Rotulo>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Rotulo from './components/Rotulo.vue'
+import NomeCompleto from "./components/NomeCompleto.vue";
 
 export default {
-	name: 'app',
-	components: { Rotulo }
-}
+  name: "app",
+  components: {NomeCompleto, Rotulo },	
+  data() {
+    return {
+	  enviado: false,
+	  nomeCompleto: {
+		nome: "",
+      	sobrenome: "",
+	  },
+      email: "",
+      senha: "",
+      armazenarDados: true
+    };
+  },
+  matheds: {
+	  enviar() {
+		  this.enviado = true
+	  }
+  }
+};
 </script>
 
 <style>
-
 body {
-	background-color: #ECECEC;
+  background-color: #ececec;
 }
 
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 
-	display: flex;
-	flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .conteudo {
-	display: flex;
+  display: flex;
 }
 
 .painel {
-	flex: 1;
-	background: #FFF;
-	margin: 0px 10px;
-	padding: 20px;
-	border: 1px solid #AAA;
-	border-radius: 5px;
+  flex: 1;
+  background: #fff;
+  margin: 0px 10px;
+  padding: 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
 }
 
 .painel .cabecalho {
-	width: 100%;
-	background-color: #DDD;
-	padding: 10px 0px;
-	border-radius: 5px;
-	font-size: 1.4rem;
+  width: 100%;
+  background-color: #ddd;
+  padding: 10px 0px;
+  border-radius: 5px;
+  font-size: 1.4rem;
 }
 
 #app form button {
-	float: right;
-	margin: 10px 0px;
-	padding: 10px 20px;
-	font-size: 1.4rem;
-	border-radius: 5px;
-	color: #FFF;
-	background-color: #2196F3;
+  float: right;
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
 }
 
 #app h1 {
-	font-weight: 200;
-	margin: 20px;
-	padding: 0;
+  font-weight: 200;
+  margin: 20px;
+  padding: 0;
 }
 
 .mr-4 {
-	margin-right: 40px;
+  margin-right: 40px;
 }
 </style>
---
+++ 10-formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input type="text" @inpur="alterarNome"
+       :value="value.nome">
+    </Rotulo>
+    <Rotulo sobrenome="Sobrenome">
+      <input type="text" @input="alterarSobrenome"
+      :value="value.sobrenome">
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./Rotulo.vue";
+
+export default {
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  components: { Rotulo },
+  methods: {
+    alterarNome(event) {
+      this.$emit("input", {
+        nome: event.target.value,
+        sobrenome: this.sobrenome
+      });
+    },
+    alterarSobrenome(event) {
+      this.$emit("input", { nome: this.nome,
+       sobrenome: event.target.value });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
