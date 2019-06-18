3b3c0d1a50758164f444dca4619e3d88fbd12164
Marcelo <marcelo.souza@dce.ufpb.br>
2019-06-17 21:38:32 -0300

    Registrando uma diretiva local

--- 11-diretivas/diretivas-exercicios/src/App.vue
+++ 11-diretivas/diretivas-exercicios/src/App.vue
@@ -1,18 +1,38 @@
 <template>
-	<div id="app">
-		<h1>Diretivas</h1>
-		<hr>
-		<p v-text="'Usando diretiva <strong>v-html</strong>'"></p>
-		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
-		<hr>
-		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
-		<p v-destaque="cor">Usando diretiva personalizada</p>
-
-	</div>
+  <div id="app">
+    <h1>Diretivas</h1>
+    <hr>
+    <p v-text="'Usando diretiva <strong>v-html</strong>'"></p>
+    <p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+    <hr>
+    <p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
+    <p v-destaque="cor">Usando diretiva personalizada</p>
+	<hr>
+    <p v-destaque-local:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
+    <p v-destaque-local="cor">Usando diretiva personalizada</p>
+  </div>
 </template>
 
 <script>
 export default {
+	componets: { },
+	directives: {
+		'destaque-local': {
+			bind(el, binding, vnode) {
+		//el.style.backgroundColor = 'lightgreen'
+
+		let atraso = 0
+		if (binding.modifiers['atrasar']) atraso = 3000
+
+		setTimeout(() => {
+			if (binding.arg === 'fundo') {
+				el.style.backgroundColor = binding.value
+			} else {
+				el.style.color = binding.value
+			}
+		}, atraso)
+		}
+	 },
 	data() {
 		return {
 			cor: 'red'
@@ -23,12 +43,12 @@ export default {
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
 }
 </style>
